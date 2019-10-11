ff7cb7702dd2e752dd494a81c666f1a432aba570
amber reese <amreese98@gmail.com>
2019-10-10 22:33:14 -0500

    added pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ContactComponent,
+    
   ],
   imports: [
     BrowserModule,
